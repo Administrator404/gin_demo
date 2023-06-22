8c8bb37d79614ab56dd5de60d0aaf187ac58f893
hyh <964808504@qq.com>
2023-06-22 10:46:13 +0800

    修正路由

--- main.go
+++ main.go
@@ -1,12 +1,9 @@
 package main
 
 import (
-	"fmt"
 	"gin_demo/config"
-	"gin_demo/dao"
+	"gin_demo/service"
 	"github.com/gin-gonic/gin"
-	"net/http"
-	"strconv"
 )
 
 func main() {
@@ -15,27 +12,7 @@ func main() {
 	// 启动gin引擎
 	r := gin.Default()
 	// 路由
-	r.GET("/test", func(c *gin.Context) {
-		// 获得size和page,此时都是string类型
-		sizeS, pageS := c.Query("size"), c.Query("page")
-		// 将size和page转为int类型
-		size, err01 := strconv.Atoi(sizeS)
-		if err01 != nil {
-			fmt.Println("size输入有误：", err01)
-		}
-		page, err02 := strconv.Atoi(pageS)
-		if err02 != nil {
-			fmt.Println("page输入有误：", err02)
-		}
-
-		// 根据size和page查询数据库记录
-		inv := dao.InvoiceQuerys(page, size)
-		//invs := inv.InvoiceQuery(page, size)
-		// 返回记录
-		c.JSON(http.StatusOK, gin.H{
-			"invs”": inv,
-		})
-	})
+	r.GET("/test", service.GetInvs)
 	// 运行服务端
 	r.Run("127.0.0.1:8080")
 }
--- service/invService.go
+++ service/invService.go
@@ -1,11 +1,32 @@
 package service
 
 import (
+	"fmt"
 	"gin_demo/dao"
-	"gin_demo/models"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"strconv"
 )
 
-func GetInvs(page int, size int) (invs []models.Invoice) {
-	invs = dao.InvoiceQuerys(page, size)
-	return invs
+func GetInvs(c *gin.Context) {
+	// 获得size和page,此时都是string类型
+	sizeS, pageS := c.Query("size"), c.Query("page")
+	// 将size和page转为int类型
+	size, err01 := strconv.Atoi(sizeS)
+	if err01 != nil {
+		fmt.Println("size输入有误：", err01)
+	}
+	page, err02 := strconv.Atoi(pageS)
+	if err02 != nil {
+		fmt.Println("page输入有误：", err02)
+	}
+
+	// 根据size和page查询数据库记录
+	inv := dao.InvoiceQuerys(page, size)
+	//invs := inv.InvoiceQuery(page, size)
+	// 返回记录
+	c.JSON(http.StatusOK, gin.H{
+		"invs”": inv,
+	})
+	return
 }
